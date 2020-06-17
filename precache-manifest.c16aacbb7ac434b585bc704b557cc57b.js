eb8e945547d3e34688decd5138ab2f3dbd42aea4
zhgromov <eyasnij@gmail.com>
2020-06-17 20:19:58 +0300

    test

---
+++ precache-manifest.c16aacbb7ac434b585bc704b557cc57b.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a06ed4afbd0a4a0dbf02f87aa392600",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7175ad508316e6d75355",
+    "url": "/static/css/main.8c714533.chunk.css"
+  },
+  {
+    "revision": "0d3e23233ed82b114941",
+    "url": "/static/js/2.7b560736.chunk.js"
+  },
+  {
+    "revision": "df35cbdf3e538a6701e9d3eb68fad19e",
+    "url": "/static/js/2.7b560736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7175ad508316e6d75355",
+    "url": "/static/js/main.aecfe524.chunk.js"
+  },
+  {
+    "revision": "32b08fe1caf5ba1d339b",
+    "url": "/static/js/runtime-main.86b9ab49.js"
+  },
+  {
+    "revision": "a7361eb5052ba4bf1987347c1d75a10c",
+    "url": "/static/media/account.a7361eb5.png"
+  },
+  {
+    "revision": "b4dc9e961da0278aef222ee5ca94b61a",
+    "url": "/static/media/accountant.b4dc9e96.png"
+  },
+  {
+    "revision": "8046f1400f3dfde68fe129a9c34add96",
+    "url": "/static/media/back3.8046f140.jpg"
+  },
+  {
+    "revision": "4344c00e57e87b12ac15d40097a2ac94",
+    "url": "/static/media/bank.4344c00e.png"
+  },
+  {
+    "revision": "486af2524db13644a1576a5fd5c426c5",
+    "url": "/static/media/biometric.486af252.png"
+  },
+  {
+    "revision": "709e83ea89364dccda191eaab5c7dd9c",
+    "url": "/static/media/bitcoin.709e83ea.png"
+  },
+  {
+    "revision": "64b575f99673fcd08af7e2ae490dab54",
+    "url": "/static/media/clock.64b575f9.png"
+  },
+  {
+    "revision": "372dc0b0a0fa892820764f31a20dd1fd",
+    "url": "/static/media/contract1.372dc0b0.png"
+  },
+  {
+    "revision": "fea520c03ec8cf190ab9e639753cbaa1",
+    "url": "/static/media/crypto.fea520c0.png"
+  },
+  {
+    "revision": "592cfcf26efafbbd0f63a133ed472f5a",
+    "url": "/static/media/fanclear.592cfcf2.png"
+  },
+  {
+    "revision": "e429814350b1bb7753c38dfe4aba3ffc",
+    "url": "/static/media/idea.e4298143.png"
+  },
+  {
+    "revision": "57b2ecc6d59ee002a5c8374fdb25e682",
+    "url": "/static/media/list.57b2ecc6.png"
+  },
+  {
+    "revision": "9a744e39eb4165de7782546b55cc01b4",
+    "url": "/static/media/minus.9a744e39.png"
+  },
+  {
+    "revision": "a145c168afc0d461259063397c36481a",
+    "url": "/static/media/minus.a145c168.png"
+  },
+  {
+    "revision": "79581676f3856150087d168e54d57e5f",
+    "url": "/static/media/money.79581676.png"
+  },
+  {
+    "revision": "fa77c63e7a9cacaf3121be80b0923f89",
+    "url": "/static/media/money.fa77c63e.png"
+  },
+  {
+    "revision": "53ac1cdf052dc3fe193c3035fe6b4577",
+    "url": "/static/media/money1.53ac1cdf.png"
+  },
+  {
+    "revision": "3e1159fff253411519352dc9219457c2",
+    "url": "/static/media/partners.3e1159ff.png"
+  },
+  {
+    "revision": "f82885587444161eae52a6e6f9f2375c",
+    "url": "/static/media/plus.f8288558.png"
+  },
+  {
+    "revision": "fca4768d8f158fd8c70c8e549a1c6f3d",
+    "url": "/static/media/plus.fca4768d.png"
+  },
+  {
+    "revision": "b9c55c1a4202a80a4fb9706a910ad00c",
+    "url": "/static/media/priceImg.b9c55c1a.png"
+  },
+  {
+    "revision": "e8f912bbbe457049fdfefce0019baba2",
+    "url": "/static/media/quote.e8f912bb.png"
+  },
+  {
+    "revision": "8ba9b32c2d9b60d2488cc87e24a218e7",
+    "url": "/static/media/safe-cloud.8ba9b32c.png"
+  },
+  {
+    "revision": "f0929f26e7491df465ee8f861565bbe2",
+    "url": "/static/media/search.f0929f26.png"
+  },
+  {
+    "revision": "7e98d3434256330ab225982805f2889e",
+    "url": "/static/media/security.7e98d343.png"
+  },
+  {
+    "revision": "7493852fe7c8e9dfada3f0cfb7a88753",
+    "url": "/static/media/test.7493852f.png"
+  },
+  {
+    "revision": "8ae0d1435bee765d87dc783d7469a9fe",
+    "url": "/static/media/transfer.8ae0d143.png"
+  },
+  {
+    "revision": "f7c2f3a38653bba8cd3bde8b02db063f",
+    "url": "/static/media/wallet.f7c2f3a3.png"
+  },
+  {
+    "revision": "73b62a1786104e8095e1820346cae734",
+    "url": "/static/media/worker.73b62a17.png"
+  }
+]);
